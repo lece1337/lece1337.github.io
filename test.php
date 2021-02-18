e999e4464b6e8f178b74eafed4da8d5a7cb0c03d
lece aka Salem <56590958+lece1337@users.noreply.github.com>
2021-02-18 19:27:54 +0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<html>
+    <head>
+        <title>Archie</title>
+    </head>
+    <body>
+        <h1>Archie's Website</h1>
+        <p>This is my website. No CSS or JavaScript or trackers or analytics. An imitation of how the web used to be.</p>
+        <p>Here's some other places on the internet that you can find me:</p>
+        <ul>
+            <li><a href="https://twitter.com/abcdefghidek">Twitter</a></li>
+            <li><a href="https://git.alles.cx/archie">Alles Code Server</a></li>
+            <li><a href="https://getmakerlog.com/@archie">Makerlog</a></li>
+            <li><a href="https://github.com/archiecodes">GitHub</a></li>
+        </ul>
+    </body>
+</html>
